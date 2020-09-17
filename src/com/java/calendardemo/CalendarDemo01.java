6e934addedf3f0fdf2671533e60c8ab0cc062c1e
qingjiawen <1503927265@qq.com>
2020-09-17 18:27:16 +0800

    calendar

--- src/com/java/calendardemo/CalendarDemo01.java
+++ src/com/java/calendardemo/CalendarDemo01.java
@@ -1,2 +1,21 @@
-package com.java.calendardemo;public class CalendarDemo01 {
+package com.java.calendardemo;
+
+import javax.jws.soap.SOAPBinding;
+import java.util.Calendar;
+
+/**
+ * 日历类 Calendar
+ */
+public class CalendarDemo01 {
+    public static void main(String[] args) {
+        //获取日历对象
+        Calendar calendar=Calendar.getInstance();
+        //获取年
+        int year=calendar.get(calendar.YEAR);
+        System.out.println(year);
+        //获取月
+        int month=calendar.get(calendar.MONTH);
+        System.out.println(month+1);
+
+    }
 }
